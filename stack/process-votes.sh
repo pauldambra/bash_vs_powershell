a7d94314c97c957ed8c7e39ac25107930fc30ba0
pauldambra <pauldambra@gmail.com>
2016-08-25 23:29:28 +0100

    take bash script work out of a subshell and simplify things a little

--- stack/process-votes.sh
+++ stack/process-votes.sh
@@ -2,54 +2,27 @@
 
 set -e
 
-TWENTYTENREGEX="CreationDate=\"2010"
+non_data_lines_count=3
+lines=$(( $(wc -l < $1) - $non_data_lines_count ))
 
-LINES=$(wc -l < $1)
-TWENTYTENLINES=$(grep $TWENTYTENREGEX $1)
+twenty_ten_regex="CreationDate=\"2010"
+vote_type_regex="VoteTypeId=\"([0-9]+)\""
 
-wait
+declare -a vote_types
 
-VOTETYPEFILE=$(mktemp)
-trap "rm -f $temp_file" 0 2 3 15
+while read line ; do
+    if [[ $line =~ $vote_type_regex ]]; then 
+        vote_type_id="${BASH_REMATCH[1]}"
+        ((vote_types[vote_type_id]++))
+    fi
+done < <(grep $twenty_ten_regex $1)
 
-(
-	declare -a VOTETYPES
+twenty_ten_count=0
+for k in "${!vote_types[@]}"
+do
+  twenty_ten_count=$(($twenty_ten_count + vote_types[$k]))
+  echo "VoteTypeId: $k had ${vote_types[$k]} votes"
+done
 
-	while read line ; do
-	    voteTypeRegex="VoteTypeId=\"([0-9]+)\""
-	    if [[ $line =~ $voteTypeRegex ]]; then 
-	        VoteTypeId="${BASH_REMATCH[1]}"
-	        ((VOTETYPES[VoteTypeId]++))
-	    fi
-	done <<<"$TWENTYTENLINES"
-
-	for k in "${!VOTETYPES[@]}"
-	do
-	  echo "VoteTypeId: $k had ${VOTETYPES[$k]} votes\n" >> $VOTETYPEFILE 
-	done
-)
-
-wait
-
-TWENTYTENCOUNT=$(echo "$TWENTYTENLINES" | wc -l)
-
-echo -e $(cat $VOTETYPEFILE)
-echo "there were $LINES total votes in the file"
-echo "there were $TWENTYTENCOUNT total votes in 2010"
-
-#  `time ./process-votes.sh ~/Downloads/Votes.xml`
-# VoteTypeId: 1 had 467439 votes
-#  VoteTypeId: 2 had 4198861 votes
-#  VoteTypeId: 3 had 256759 votes
-#  VoteTypeId: 4 had 200 votes
-#  VoteTypeId: 5 had 380887 votes
-#  VoteTypeId: 8 had 8632 votes
-#  VoteTypeId: 9 had 8578 votes
-#  VoteTypeId: 10 had 199344 votes
-#  VoteTypeId: 11 had 13231 votes
-#  VoteTypeId: 12 had 535 votes
-#  VoteTypeId: 15 had 965 votes
-
-# there were  105301747 total votes in the file
-# there were  5535431 total votes in 2010
-# ./process-votes.sh ~/Downloads/Votes.xml  719.24s user 55.83s system 101% cpu 12:46.44 total
+echo "there were $lines total votes in the file"
+echo "there were $twenty_ten_count total votes in 2010"
